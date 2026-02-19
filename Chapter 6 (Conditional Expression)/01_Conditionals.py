06a91ca46dfd4de248e566e63eca5f3469b7dee4
SACHIN KUMAR <ersachin.ai@gmail.com>
2026-02-19 22:50:47 +0530

    First Conditional

---
+++ Chapter 6 (Conditional Expression)/01_Conditionals.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+
+if(a>=18):
+    print("You are an adult.")
+    print("You can vote.")
+
+else:
+    print("You are below the age of 18. You are not an adult.")
+    print("You cannot vote.")
